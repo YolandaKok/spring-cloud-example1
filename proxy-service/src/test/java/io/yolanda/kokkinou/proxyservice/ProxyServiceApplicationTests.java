e94315540a4b1ae841e91cf146c1ce58780e6814
YolandaKok <yolanda.kokkinou@gmail.com>
2020-07-28 15:48:38 +0300

    proxy-service and user-service

---
+++ proxy-service/src/test/java/io/yolanda/kokkinou/proxyservice/ProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yolanda.kokkinou.proxyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
