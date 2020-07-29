24de2ff0a97175a17260c1744eb72e5df6101b87
YolandaKok <yolanda.kokkinou@gmail.com>
2020-07-29 19:15:40 +0300

    exclude a dependency which forces xml response

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -22,8 +22,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	implementation("com.google.code.gson:gson:2.8.2")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client") {
+		exclude(group = "com.fasterxml.jackson.dataformat", module = "jackson-dataformat-xml")	
+	}
+	//implementation("com.google.code.gson:gson:2.8.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
--- user-service/src/main/kotlin/io/yolanda/kokkinou/endpoints/UserEndpoint.kt
+++ user-service/src/main/kotlin/io/yolanda/kokkinou/endpoints/UserEndpoint.kt
@@ -5,17 +5,15 @@ import io.yolanda.kokkinou.dto.ReturnValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.http.MediaType
-import com.google.gson.Gson
 
 @RestController
 @RequestMapping("/users")
 class UserEndpoint {
-	@RequestMapping("/")
-	fun getUsers(): String {
+	
+	@RequestMapping
+	fun getUsers(): ReturnValue {
 		val result = ReturnValue()
 		result.listOfNames = "Yolanda, Maria, Eleni"
-		val jsonString = Gson().toJson(result)
-		
-		return jsonString
+		return result
 	}	
 }
