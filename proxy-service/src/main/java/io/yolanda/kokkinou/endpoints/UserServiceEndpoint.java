42ae7a9d96df8940b80d4e6f671164a847a81678
YolandaKok <yolanda.kokkinou@gmail.com>
2020-07-31 14:56:38 +0300

    Hystrix fault tolerance

--- proxy-service/src/main/java/io/yolanda/kokkinou/endpoints/UserServiceEndpoint.java
+++ proxy-service/src/main/java/io/yolanda/kokkinou/endpoints/UserServiceEndpoint.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import io.yolanda.kokkinou.dto.UsersDTO;
 import io.yolanda.kokkinou.proxy.UserServiceProxy;
 
@@ -21,7 +23,16 @@ public class UserServiceEndpoint {
 	}
 	
 	@GetMapping("/test")
+	@HystrixCommand(fallbackMethod = "defaultUserList")
 	public UsersDTO getUsers() {
 		return proxy.getUsers();
 	}
+	
+	public UsersDTO defaultUserList() {
+		String defaultUsers = "George, Mary, Oliver";
+		UsersDTO response = new UsersDTO();
+		response.setListOfNames(defaultUsers);
+		return response;
+	}
+	
 }
